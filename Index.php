c39e178fea8a8b98754a0765d6095f8fcf067f78
Bevilaquas <thelostsinner2009@gmail.com>
2026-08-14 22:17:05 -0300

    Bruto da index finalizado, adicionar alguns detalhes a mais posteriormente

--- Index.php
+++ Index.php
@@ -12,9 +12,11 @@
 </head>
 <body>
 
+    <img src="assets/img/Logo.png" id="logo">
+
     <nav class="links">
 
-    <a href="View/cliente/Home.php"> INÍCIO </a>
+    <a href="View/cliente/Home.php" class="ativo"> INÍCIO </a>
     <a href="View/cliente/Servicos.php"> SERVIÇOS </a>
     <a href="View/cliente/Funcionarios.php"> EQUIPE </a>
     <a href="View/cliente/Home.php"> CONTATO </a>
@@ -24,6 +26,26 @@
 
     <div id="fundolinks"> </div>
 
-    
+    <h2  id="TxtMenor"> BELEZA | CONFIANÇA | ELEGÂNCIA </h2>
+    <h1 id="TxtMaior"> Realçando sua <span class="Beleza"> Beleza </span> <br> com <span class="Beleza"> Elegância </span></h1>
+
+    <div id="linha"> </div>
+
+    <h2 id="descricao"> Serviços de Qualidade para Realçar <br> a sua Beleza no dia a dia </h2>
+
+    <a href="View/cliente/Servicos.php" id="btn-servicos"> conheça nossos serviços! </a>
+
+    <div id="separadorRodape"> </div>  
+    <div id="fundoRodape"> </div>
+
+    <h2 id="textoRodape"> AGENDE SEU HORÁRIO </h2>
+
+    <div id="linhaRodape"> </div>
+
+    <a href="" id="btn-agendar"> AGENDAR AGORA </a>
+
+    <div id="marcadorImagem"> </div>
+    <h1 id="texto"> Espaço Reservado pra Imagens do Local </h1>
+
 </body>
 </html>
